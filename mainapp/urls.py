2078e6434c5728db884bb4e2240602f3b9b49c21
symons <somborisymonss@gmail.com>
2019-10-17 21:04:48 +0300

    done with dashboard1 and begun dashboard 2

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,4 +4,6 @@ from mainapp.views import mainview,pastors
 urlpatterns = [
     path('', mainview.index, name='index'),
     path('pastor/dashboard/home', pastors.pastorHome, name = 'pastors_home'),
+    path('add/group', pastors.GroupCreateView.as_view(), name = 'add_group'),
+    path('pastor/dashboard/group', pastors.groupView, name = 'pastor_groups')
 ]
--- mainapp/views/pastors.py
+++ mainapp/views/pastors.py
@@ -3,11 +3,12 @@ from django.shortcuts import redirect
 from django.views.generic import CreateView
 from mainapp.forms import PastorSignupForm
 from django.shortcuts import render
-from ..models import User
+from ..models import User,Group
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils.decorators import method_decorator
 from ..decorators import pastor_required
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 class PastorSignUpView(CreateView):
     model = User
@@ -26,3 +27,25 @@ class PastorSignUpView(CreateView):
 def pastorHome(request):
     staff = User.objects.all()
     return render(request,  'pastor/dashboard1-staff.html', {'staff':staff})
+
+def groupView(request):
+    groups = Group.objects.all()
+    return render(request, 'pastor/dashboard2-groups.html', {'groups':groups})
+
+class GroupCreateView(CreateView):
+    model = Group
+    fields = ('name','leader')
+    template_name = 'pastor/group_add_form.html'
+
+    def form_valid(self, form): 
+        group = form.save(commit=False)
+        # tgroup.added_by = self.request.user
+        group.save()
+        messages.success(self.request, 'Group Succesfully Created!')
+        if self.request.user.is_pastor:
+            return redirect('pastors_home')
+
+        else:
+            return redirect('index')
+
+        
